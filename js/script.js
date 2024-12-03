c1fac3640e1ed0c2310d98a3d7a129d0058e5f3f
matteodragotto <matteodrag8@gmail.com>
2024-12-03 15:14:21 +0100

    [feat] getMail js function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 
 
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const mailList = [];
 
+
+function getMail() {
+
+  axios.get(endpoint)
+  .then(response => {
+  console.log(response.data);
+  if (response.data.success){
+    const email = response.data.response;
+    console.log(email);
+  }  
+  
+  })
+
+  .catch(err => {
+    console.log(err);
+    printMessage('Errore! ' + err.message)
+  })
+}
+
+
+
+  
