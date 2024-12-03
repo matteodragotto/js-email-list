323859353f5e7ac2cf5f16ea345c7a2e68210e1f
matteodragotto <matteodrag8@gmail.com>
2024-12-03 17:00:23 +0100

    [feat] button to refresh email list

--- js/script.js
+++ js/script.js
@@ -2,40 +2,51 @@
 
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const ul = document.getElementById('mails')
-let mail;
 const mailNumber = 0;
+const emailList = []
+const btn = document.getElementById('refresh')
 
+refresh();
 
-function getMail() {
+btn.addEventListener('click', refresh)
 
-  axios.get(endpoint)
-  .then(response => {
-    if (response.data.success){
-      const email = response.data.response;
-      printMail(email)
+function refresh () {
+  ul.innerHTML = ''
 
-    }  
+  function getMail() {
+
+    axios.get(endpoint)
+    .then(response => {
+      if (response.data.success){
+        const email = response.data.response;      
+        
+        printMail(email)      
   
-  })
-
-  .catch(err => {
-    console.log(err);
-  })
-}
-
-
-function randomMail(counter) {
-  while (counter < 10) {
-    getMail()
-    counter++
+      }  
+    
+    })
+  
+    .catch(err => {
+      console.log(err);
+    })
   }
   
+  
+  function randomMail(counter) {
+    while (counter < 10) {
+      getMail()
+      counter++
+    }
+    
+  }
+  
+  function printMail (mail) {
+    let li = document.createElement('li')
+    li.innerText = mail
+    ul.append(li)
+  }
+    
+  randomMail(mailNumber)
 }
 
-function printMail (mail) {
-  let li = document.createElement('li')
-  li.innerText = mail
-  ul.append(li)
-}
-  
-randomMail(mailNumber)
+
